4e9bedd4e42ca42ec67dc87f6fdbdd53a3973f27
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2024-01-31 09:39:46 +0100

    Pushes progress for help L13

--- wicketcompact/src/main/java/nl/suzannawentzel/wicketcompact/Header.java
+++ wicketcompact/src/main/java/nl/suzannawentzel/wicketcompact/Header.java
@@ -1,6 +1,7 @@
 package nl.suzannawentzel.wicketcompact;
 
 import nl.suzannawentzel.wicketcompact.resources.LogoResourceReference;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
@@ -10,13 +11,30 @@ import java.awt.print.Book;
 
 public class Header extends Panel
 {
+	private final MarkupContainer dashboardLink;
+	private final MarkupContainer categoriesLink;
+	private final MarkupContainer itemsLink;
+	private final MarkupContainer tablesLink;
+
 	public Header(String id) {
 		super(id);
 //		add(new BookmarkablePageLink<Void>("logo", getApplication().getHomePage()));
-		add(new BookmarkablePageLink<Void>("dashboard", getApplication().getHomePage()));
-		add(new BookmarkablePageLink<Void>("categories", CategoriesPage.class));
-		add(new BookmarkablePageLink<Void>("items", ItemsPage.class));
-		add(new BookmarkablePageLink<Void>("tables", TablesPage.class));
+		dashboardLink = add(new BookmarkablePageLink<Void>("dashboard", getApplication().getHomePage()));
+		categoriesLink = add(new BookmarkablePageLink<Void>("categories", CategoriesPage.class));
+		itemsLink = add(new BookmarkablePageLink<Void>("items", ItemsPage.class));
+		tablesLink = add(new BookmarkablePageLink<Void>("tables", TablesPage.class));
+	}
+
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+
+		System.err.println(getPage().getClass());
+		dashboardLink.setEnabled(!getPage().getClass().equals(HomePage.class));
+		categoriesLink.setEnabled(!getPage().getClass().equals(CategoriesPage.class));
+		itemsLink.setEnabled(!getPage().getClass().equals(ItemsPage.class));
+		tablesLink.setEnabled(!getPage().getClass().equals(TablesPage.class));
+
 		add(new Image("brand", new LogoResourceReference()));
 	}
 }
--- wicketcompact/src/main/java/nl/suzannawentzel/wicketcompact/resources/BootstrapJsResourceReference.java
+++ wicketcompact/src/main/java/nl/suzannawentzel/wicketcompact/resources/BootstrapJsResourceReference.java
@@ -1,6 +1,12 @@
 package nl.suzannawentzel.wicketcompact.resources;
 
+import org.apache.wicket.Application;
+import org.apache.wicket.markup.head.HeaderItem;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
+
+import java.util.List;
 
 public class BootstrapJsResourceReference extends PackageResourceReference
 {
@@ -12,5 +18,13 @@ public class BootstrapJsResourceReference extends PackageResourceReference
 
 	public static BootstrapJsResourceReference get() {
 		return INSTANCE;
+	}
+
+	@Override
+	public List<HeaderItem> getDependencies() {
+		final List<HeaderItem> dependencies = super.getDependencies();
+		final ResourceReference jQueryReference = Application.get().getJavaScriptLibrarySettings().getJQueryReference();
+		dependencies.add(JavaScriptHeaderItem.forReference(jQueryReference));
+		return dependencies;
 	}
 }
