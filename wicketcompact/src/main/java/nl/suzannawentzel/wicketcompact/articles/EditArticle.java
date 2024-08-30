d1903fc211dada72ab2ed2b5f5cbe902504e5c59
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2024-08-30 15:49:46 +0200

    S9-L55: refreshes parts of form on update with AJAX

--- wicketcompact/src/main/java/nl/suzannawentzel/wicketcompact/articles/EditArticle.java
+++ wicketcompact/src/main/java/nl/suzannawentzel/wicketcompact/articles/EditArticle.java
@@ -7,15 +7,20 @@ import nl.suzannawentzel.wicketcompact.entities.Category;
 import nl.suzannawentzel.wicketcompact.models.EntityModel;
 import nl.suzannawentzel.wicketcompact.services.ArticleService;
 import nl.suzannawentzel.wicketcompact.services.ServiceRegistry;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.bean.validation.PropertyValidator;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableChoiceLabel;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableMultiLineLabel;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.FormComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.ExternalLink;
@@ -41,13 +46,55 @@ public class EditArticle extends Panel
 		}
 	};
 
-	private final AjaxEditableLabel<String> nameField;
+	private final TextField<String> nameField;
+
+	private final MarkupContainer image;
+
+	private final FormComponent<String> imageUrl;
+
+	private final ExternalLink helpLink;
 
 	public EditArticle(String id)
 	{
 		super(id);
 		form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
-		this.nameField = new AjaxEditableLabel<>("name");
+		this.nameField = new TextField<>("name");
+		helpLink = new ExternalLink("help", new Model<String>()
+		{
+			@Override
+			public String getObject()
+			{
+				return "https://wikipedia.org/wiki/" + EditArticle.this.nameField.getModelObject();
+			}
+		});
+
+		helpLink.setOutputMarkupId(true);
+
+		nameField.add(new AjaxFormComponentUpdatingBehavior("change") {
+			@Override
+			protected void onUpdate(AjaxRequestTarget target)
+			{
+				target.add(helpLink);
+			}
+		});
+		this.image = new WebMarkupContainer("image");
+		this.image.add(new AttributeModifier("src", new Model<String>() {
+			@Override
+			public String getObject()
+			{
+				return imageUrl.getModelObject();
+			}
+		}));
+		image.setOutputMarkupId(true);
+
+		imageUrl = new TextField<String>("imageUrl").add(new PropertyValidator<>()).setLabel(Model.of("Image URL"));
+		imageUrl.add(new AjaxFormComponentUpdatingBehavior("change") {
+			@Override
+			protected void onUpdate(AjaxRequestTarget target)
+			{
+				target.add(image);
+			}
+		});
 	}
 
 	@Override
@@ -60,20 +107,15 @@ public class EditArticle extends Panel
 	private void initializeForm() {
 		add(form);
 		add(new ValidationErrorFeedbackPanel("validationFeedback"));
-		form.add(nameField);
+		form.add(nameField.add(new PropertyValidator<>()));
 		form.add(new AjaxEditableMultiLineLabel<>("description").setLabel(Model.of("Description")));
 		form.add(new AjaxEditableChoiceLabel<>("category", null, new CategoryListModel(), new ChoiceRenderer<>("name", "id")));
 		form.add(new AjaxEditableLabel<>("price").setLabel(Model.of("Price")));
 		form.add(new AjaxEditableLabel<>("validFrom").setLabel(Model.of("Valid from")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
 		form.add(new AjaxEditableLabel<>("validTo").setLabel(Model.of("Valid to")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
-		form.add(new AjaxEditableLabel<>("imageUrl").setLabel(Model.of("Image URL")));
-		form.add(new ExternalLink("help", new Model<String>() {
-			@Override
-			public String getObject()
-			{
-				return "https://de.wikipedia.org/wiki/" + EditArticle.this.nameField.getModelObject();
-			}
-		}));
+		form.add(imageUrl);
+		form.add(image);
+		form.add(this.helpLink);
 	}
 
 	void setArticle(Article article) {
